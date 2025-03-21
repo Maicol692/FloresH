51e5c59773fd09fd006d0349170daffd0523ee26
Maicol692 <maicol692@gmail.com>
2025-03-21 02:20:03 -0500

    Primer commit - Subiendo proyecto

--- DATOS/main.js
+++ DATOS/main.js
@@ -1,3 +1,12 @@
-onload = () =>{
+onload = () => {
         document.body.classList.remove("container");
-};
+    };
+    
+    // Esperar a que el usuario haga clic para reproducir la música
+    document.addEventListener("click", () => {
+        const audio = document.querySelector("audio");
+        if (audio) {
+            audio.play().catch(error => console.log("Error al reproducir el audio:", error));
+        }
+    });
+    
